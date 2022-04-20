0b41cb073076bbfac0457bce2db5ca5c29f4a6a5
hwseo0705 <julieseo0705@gmail.com>
2022-04-20 18:18:39 +0900

    [04.20.22] JS String Commit

---
+++ 1.core-js/2.dataType/2.string.js
@@ -0,0 +1,28 @@
+var greeting;
+greeting = 'Hello';
+greeting = "Hola";
+greeting = `Bonjour`;
+
+// these are all string type
+console.log(typeof greeting);
+
+// they have many ways to show string, in case you want to do the following
+// where you add quotation marks in your string
+var str1 = "He said his name was 'Jack' and ran away";
+console.log(str1);
+
+var str2 = `Let's Go!`;
+console.log(str2);
+
+var str3 = 'Let\'s Go!!!!!!!';
+console.log(str3);
+
+// path for linux is '/', path for windows is '\'
+var str4 = 'E:\\temp\\new.png';
+console.log(str4);
+
+var str5 = 'doggo\n\n\n\nmeow';
+console.log(str5);
+
+var str6 = 'doggo\t\t\tmeow';
+console.log(str6);
